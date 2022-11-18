a6286b080989db044686866abde3dc742b53354a
Michael Scherer <misc@redhat.com>
2022-11-19 00:30:09 +0100

    Use fmt

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ fn get_joycon() -> evdev::Device {
     // productid 8198
     // TODO use https://github.com/emberian/evdev/blob/master/examples/_pick_device.rs
     let mut c = 0;
-    let devices = evdev::enumerate()
-            .collect::<Vec<_>>();
-        // readdir returns them in reverse order from their eventN names for some reason
+    let devices = evdev::enumerate().collect::<Vec<_>>();
+    // readdir returns them in reverse order from their eventN names for some reason
     for (i, d) in devices.iter().enumerate() {
         if d.input_id().product() == PRODUCT_ID && d.input_id().vendor() == VENDOR_ID {
             c = i;
@@ -18,6 +17,7 @@ fn get_joycon() -> evdev::Device {
     devices.into_iter().nth(c).unwrap()
 }
 
+/*
 fn make_vibrate(j: &mut evdev::Device) {
      for i in 0..5 {
         let on = i % 2 != 0;
@@ -31,31 +31,34 @@ fn make_vibrate(j: &mut evdev::Device) {
         .unwrap();
         std::thread::sleep(std::time::Duration::from_secs(1));
      }
-}
+} */
 
 fn press_right(ui: &mut uinput::Device) {
-    	ui.click(&uinput::event::keyboard::Key::Right).unwrap();
-        println!("Pressed right");
+    ui.click(&uinput::event::keyboard::Key::Right).unwrap();
+    println!("Pressed right");
 }
 
 fn main() {
     let mut j = get_joycon();
-    let mut ui = uinput::default().unwrap()
-		.name("test").unwrap()
-		.event(uinput::event::Keyboard::All).unwrap()
-		.create().unwrap();
+    let mut ui = uinput::default()
+        .unwrap()
+        .name("test")
+        .unwrap()
+        .event(uinput::event::Keyboard::All)
+        .unwrap()
+        .create()
+        .unwrap();
 
     println!("Device: {:?}", j.name());
-    make_vibrate(&mut j);
+    // make_vibrate(&mut j);
     loop {
         for ev in j.fetch_events().unwrap() {
             let k = ev.kind();
             match k {
                 evdev::InputEventKind::Key(_) => press_right(&mut ui),
-                _ => (),
-                //                _ => println!("{:?}", ev.kind()),
+                //_ => (),
+                _ => println!("{:?}", ev.kind()),
             }
         }
     }
-
 }
