350d5ab95e18f70ca469d5e22ec15b93cb0f7f30
Michael Scherer <misc@redhat.com>
2022-11-19 01:55:15 +0100

    Clean code, and use clippy

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,12 @@
-use evdev;
-use uinput;
-
 // Nintendo product ID
 const VENDOR_ID: u16 = 1406;
 const PRODUCT_ID: u16 = 8198;
 
 fn get_joycon() -> Option<evdev::Device> {
-    // TODO use https://github.com/emberian/evdev/blob/master/examples/_pick_device.rs
-    for d in evdev::enumerate() {
-        if d.input_id().product() == PRODUCT_ID && d.input_id().vendor() == VENDOR_ID {
-            return Some(d);
-        }
-    }
-    None
+    evdev::enumerate()
+        .map(|(_, d)| d)
+        .find(|d| d.input_id().product() == PRODUCT_ID && d.input_id().vendor() == VENDOR_ID)
 }
-
-/*
-fn make_vibrate(j: &mut evdev::Device) {
-     for i in 0..5 {
-        let on = i % 2 != 0;
-        j.send_events(&[
-            evdev::InputEvent::new(
-                evdev::EventType::FORCEFEEDBACK,
-                10,
-                if on { i32::MAX } else { 0 },
-            ),
-        ])
-        .unwrap();
-        std::thread::sleep(std::time::Duration::from_secs(1));
-     }
-} */
 
 fn press_right(ui: &mut uinput::Device) {
     ui.click(&uinput::event::keyboard::Key::Right).unwrap();
