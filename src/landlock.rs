476f3290fd6c4b93fb20b16e2c0a184919f466ac
Michael Scherer <misc@redhat.com>
2025-05-26 21:56:28 +0200

    Add a bit more restrictio (and now, we need to open them)

--- src/landlock.rs
+++ src/landlock.rs
@@ -1,4 +1,6 @@
-use landlock::{AccessFs, RestrictionStatus, Ruleset, RulesetAttr, RulesetError, ABI};
+use landlock::{
+    Access, AccessFs, AccessNet, RestrictionStatus, Ruleset, RulesetAttr, RulesetError, Scope, ABI,
+};
 
 pub struct LandlockConfiner {}
 
@@ -8,11 +10,15 @@ impl LandlockConfiner {
     }
 
     pub fn confine(&self) -> Result<RestrictionStatus, RulesetError> {
+        // TODO add prctl(PR_SET_NO_NEW_PRIVS)
         let abi = ABI::V6;
         let ruleset = Ruleset::default();
-        // TODO do a better job at confining
+        // TODO let open /dev/input
         ruleset
-            .handle_access(AccessFs::from_read(abi))?
+            .handle_access(AccessFs::from_all(abi))?
+            .handle_access(AccessNet::from_all(abi))?
+            .scope(Scope::Signal)?
+            .scope(Scope::AbstractUnixSocket)?
             .create()?
             .restrict_self()
     }
