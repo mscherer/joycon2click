31a69ec29357ed5524ff64245eab58517c9b426e
Michael Scherer <misc@redhat.com>
2025-07-09 14:34:28 +0200

    Split Clicker in a separate file

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,11 @@ const VID_NINTENDO: u16 = 1406;
 const PID_JOYCON_LEFT: u16 = 8198;
 const PID_JOYCON_RIGHT: u16 = 8199;
 
-use std::io::ErrorKind;
-use std::os::fd::AsRawFd;
 use std::process::exit;
 use std::time::Duration;
 use std::{process, thread};
 
-use evdev::{
-    uinput::{VirtualDevice, VirtualDeviceBuilder},
-    {AttributeSet, KeyCode, KeyEvent},
-};
+use evdev::KeyCode;
 
 #[cfg(feature = "sandbox")]
 use nix::sys::prctl::set_no_new_privs;
@@ -24,6 +19,7 @@ use kobject_uevent::{ActionType, UEvent};
 
 use clap::Parser;
 
+pub mod clicker;
 pub mod user_parser;
 
 #[cfg(feature = "seccomp")]
@@ -73,59 +69,6 @@ struct Cli {
     disable_sandbox: bool,
 }
 
-struct Clicker {
-    device: VirtualDevice,
-}
-
-impl Clicker {
-    fn new() -> Clicker {
-        let mut keys = AttributeSet::<KeyCode>::new();
-        keys.insert(KeyCode::KEY_LEFT);
-        keys.insert(KeyCode::KEY_RIGHT);
-
-        // TODO see what happen if uinput is not here
-        #[allow(deprecated)]
-        let device = match VirtualDeviceBuilder::new() {
-            Err(e) if e.kind() == ErrorKind::PermissionDenied => {
-                println!("Permission error on /dev/uinput.");
-                println!("Check the documentation for various workarounds.");
-                exit(1);
-            }
-            Err(e) => {
-                println!("Error: {e:?}");
-                exit(1);
-            }
-
-            Ok(d) => d
-                .name("Joycon2click virtual keyboard")
-                .with_keys(&keys)
-                .unwrap()
-                .build()
-                .unwrap(),
-        };
-        Clicker { device }
-    }
-
-    fn press_key(&mut self, keycode: KeyCode) {
-        let down_event = *KeyEvent::new(keycode, 1);
-        let up_event = *KeyEvent::new(keycode, 0);
-        self.device.emit(&[down_event, up_event]).unwrap();
-    }
-
-    fn press_left(&mut self) {
-        self.press_key(KeyCode::KEY_LEFT)
-    }
-
-    fn press_right(&mut self) {
-        self.press_key(KeyCode::KEY_RIGHT)
-    }
-
-    #[allow(dead_code)]
-    fn get_device_fd(&mut self) -> i32 {
-        self.device.as_raw_fd()
-    }
-}
-
 fn wait_for_joycon() {
     let mut socket = Socket::new(NETLINK_KOBJECT_UEVENT).unwrap();
     let sa = SocketAddr::new(process::id(), 1);
@@ -150,7 +93,7 @@ fn wait_for_joycon() {
 fn main() {
     let cli = Cli::parse();
 
-    let mut c = Clicker::new();
+    let mut c = clicker::Clicker::new();
 
     #[cfg(feature = "sandbox")]
     if !cli.disable_sandbox {
